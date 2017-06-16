765bb00c934e6d30af71be696faf6528993d7c9f
Taylor Lawson <taylorl@benon.com>
2017-06-16 12:04:36 +1000

    Removed oh my zsh configs as nixos can now manage that

--- default.nix
+++ default.nix
@@ -3,25 +3,6 @@ with import <nixhome> { inherit stdenv; inherit pkgs; };
 with lib;
 let
   ## Customizable
-  zshTheme = "flazz";
-  zshCustom = "$HOME/.config/zsh";
-  zshViMode = true;
-  zshPlugins = [
-    "git"
-    "common-aliases"
-    "jira"
-    "ssh-agent"
-    "gitfast"
-    "docker"
-    "dirhistory"
-    "per-directory-history"
-    "perms"
-    "sudo"
-    "systemd"
-    "web-search"
-    "history-substring-search"
-  ];
-  zshAutojump = true;
   preferedFont = "Source Code Pro";
   vifmTheme = "solarized-dark";
   ## Repositories
@@ -45,11 +26,6 @@ let
     rev = "025ceddbddf55f2eb4ab40b05889148aab9699fc";
     sha256 = "0lxv37gmh38y9d3l8nbnsm1mskcv10g3i83j0kac0a2qmypv1k9f";
   };
-  zshSyntaxHighlighting = fetchgit {
-    url = "https://github.com/zsh-users/zsh-syntax-highlighting.git";
-    rev = "ad522a091429ba180c930f84b2a023b40de4dbcc";
-    sha256 = "0vncgzyb0z470i2jz3997fx41h71nq6sp47qrg6w8x0y8frivvrd";
-  };
   vifmColors = fetchgit {
     url = "https://github.com/vifm/vifm-colors.git";
     rev = "235f9e8728810cfa6c0e07974dbd72ac9158f745";
@@ -63,27 +39,13 @@ in mkHome {
       [options]
       font = ${preferedFont} 10
     '' + builtins.readFile "${termiteSolarized}/solarized-dark";
-    ".zshrc".content = let
-      plugins = zshPlugins ++ optionals zshViMode [ "vi-like" "vi-mode" ];
-    in ''
+    ".zshrc".content = ''
       export EDITOR=vim
       if [ "$TERM" = 'xterm-termite' ] && ! [ -f "$HOME/.terminfo/r/xterm-termite" ]; then
         export TERM='xterm-256color'
       fi
       [ -f $HOME/.localrc ] && source $HOME/.localrc
       eval $(dircolors ${solarizedDirColors}/dircolors.ansi-dark)
-
-      ${optionalString zshAutojump "source ${autojump}/share/autojump/autojump.zsh"}
-
-      export ZSH=${oh-my-zsh}/share/oh-my-zsh/
-      ZSH_CUSTOM="${zshCustom}"
-      mkdir -p $ZSH_CUSTOM/themes
-
-      ZSH_THEME="${zshTheme}"
-      plugins=(${concatStringsSep " " plugins})
-
-      source $ZSH/oh-my-zsh.sh
-      source ${zshSyntaxHighlighting}/zsh-syntax-highlighting.zsh
     '';
     ".Xresources".content = ''
       rofi.color-enabled: true
