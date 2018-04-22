00883c1fb945c871c0484a3358a5a75a9bfd56e6
Taylor Lawson <nekroze.lawson@gmail.com>
2018-04-23 08:45:29 +1000

    Pull before cherry-pick

--- .cherry-pick.sh
+++ .cherry-pick.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 set -euf
-
 COMMIT="$(git rev-parse HEAD)"
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 TARGET="laptop"
 if [ "$BRANCH" != "master" ]; then
     TARGET="master"
 fi
-
+echo
+echo "BRANCH: $BRANCH"
+echo
+git pull --rebase
+git push
+echo
+echo "BRANCH: $TARGET"
+echo
 git checkout "$TARGET"
+git pull --rebase
 git cherry-pick "$COMMIT"
 git push
 git checkout "$BRANCH"
-git push
