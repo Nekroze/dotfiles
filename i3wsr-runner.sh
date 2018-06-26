b55c4374b4f0b1a24894163cf2eecb83d2dd06f7
Taylor Lawson <taylorl@benon.com>
2018-06-27 08:33:52 +1000

    Add logging to new i3wsr-runner.sh for i3wsr user service

--- i3wsr-runner.sh
+++ i3wsr-runner.sh
@@ -9,9 +9,11 @@ function i3_is_running() {
 	local attempts="${1:-5}"
 	until [ "$attempts" -le 0 ]; do
 		silently pgrep i3 && return 0
+		echo "i3 is not running, retrying in 1 second..."
 		sleep 1
 		attempts=$[$attempts-1]
 	done
+	echo "Cannot use i3wsr without i3, stopping."
 	return 1
 }
 
@@ -19,5 +21,6 @@ if ! i3_is_running; then
 	exit 0
 fi
 
+echo "Launching i3wsr."
 i3wsr
 exit 1
