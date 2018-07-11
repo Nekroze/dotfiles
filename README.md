8d3c034073eb55a0c21ab7255fd44b24da946ea8
Taylor Lawson <taylorl@benon.com>
2018-07-12 09:56:00 +1000

    Added support for wtf using example config

--- README.md
+++ README.md
@@ -20,6 +20,7 @@
 * [tmux](http://tmux.github.io/)
 * [multitail](http://www.vanheusden.com/multitail/)
 * [oni](https://www.onivim.io) (Due to an [issue with oni](https://github.com/onivim/oni/issues/2087) you need to use oni to edit the config and save with `:w!` before config changes are applied))
+* [wtf](https://wtfutil.com)
 
 # Usage
 
--- default.nix
+++ default.nix
@@ -225,6 +225,8 @@ in mkHome rec {
     ".vim/UtiliSnips/d.snippets" = "${vimDsnips}/d.snippets";
     ".themes/numix-solarized-dark" = gtkSolarized;
     ".local/share/applications/keybase.desktop".content = mkDesktop "Keybase" "env NIX_SKIP_KEYBASE_CHECKS=1 ${pkgs.keybase-gui}/bin/keybase-gui";
+    ".local/share/applications/wtf.desktop".content = mkDesktop "Keybase" "kitty env TERM=xterm-256color /home/${user}/.go/bin/wtf";
+    ".config/wtf/config.yml" = "${dotfiles}/wtf.yml";
     ".config/fish/conf.d/go.fish" = writeFishScript "go.fish" ''
       set -x GOPATH $HOME/.go
       set -x PATH $PATH $GOPATH/bin
