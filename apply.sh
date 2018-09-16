0efea8610c1a17874b405b3dda9cce00a8a6c87f
Taylor Nekroze Lawson <taylorl@benon.com>
2018-09-17 07:46:08 +1000

    Add new params to apply.sh

--- apply.sh
+++ apply.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 set -eux
 variant="${1:-laptop}"
-username=${2:-$USER}
-nix-home --set --arg username "\"$username\"" --arg variant "\"$variant\""
+username="${2:-$USER}"
+email="${3:-nekroze.lawson@gmail.com}"
+nix-home --set \
+	--arg username "\"$username\"" \
+	--arg email "\"$email\"" \
+	--arg variant "\"$variant\""
